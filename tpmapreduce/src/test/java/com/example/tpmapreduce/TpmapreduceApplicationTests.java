2aebc0d3699fdfde3988fb6515c8e4969a1be95e
Metzo-GH <mouhamedhino@gmail.com>
2024-03-28 09:52:51 +0100

    Creation tp Map reduce

---
+++ tpmapreduce/src/test/java/com/example/tpmapreduce/TpmapreduceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpmapreduce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpmapreduceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
