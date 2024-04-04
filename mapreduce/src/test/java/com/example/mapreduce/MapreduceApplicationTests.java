b8d6154d60d25a2181d42cd86123f7dffc6e5023
Metzo-GH <mouhamedhino@gmail.com>
2024-04-04 09:07:52 +0200

    Rebasing

---
+++ mapreduce/src/test/java/com/example/mapreduce/MapreduceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mapreduce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapreduceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
